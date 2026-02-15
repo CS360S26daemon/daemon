491ed718a64b6b9809f843a3b9fd25ded967f686
MABDz <27100242@lums.edu.pk>
2026-02-16 00:17:21 +0500

    Added Abdogon class extending Shape

---
+++ app/src/main/java/com/example/daemon/Abdogon.java
@@ -0,0 +1,19 @@
+package com.example.daemon;
+
+public class Abdogon extends Shape {
+    private String abdness;
+
+    public Abdogon(String abdness) {
+        this.abdness = abdness;
+    }
+
+    public void set_abdness(String abdness) {
+        this.abdness = abdness;
+    }
+
+    public String get_abdness() {
+        return abdness;
+    }
+
+
+}
